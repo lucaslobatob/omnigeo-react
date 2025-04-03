268bda3ca366c7e116f12cac36b6251e5d4cfec1
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-04-03 11:24:03 -0300

    stylied responsive header component and add hamburguer menu in mobile environment

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,26 +1,30 @@
+import { useState } from "react";
 import logoaut from "../../assets/img/logoaut.png";
 import "./header.css";
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <header className="header">
             <div className="header__logo">
                 <img src={logoaut} alt="Logo Omnigeo" />
             </div>
+
+            {/* Ícone do menu hamburguer */}
+            <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </div>
+
+            {/* Menu principal */}
             <nav>
-                <ul class="menu">
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#quem-somos">Quem Somos</a>
-                    </li>
-                    <li>
-                        <a href="#servicos">Serviços</a>
-                    </li>
-                    <li class="dropdown">
+                <ul className={`menu ${menuOpen ? "active" : ""}`}>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#quem-somos">Quem Somos</a></li>
+                    <li><a href="#servicos">Serviços</a></li>
+                    <li className="dropdown">
                         <a href="#obras">Obras</a>
-                        <ul class="submenu">
+                        <ul className="submenu">
                             <li><a href="#obras">Reservatórios de Água</a></li>
                             <li><a href="#obras">Reservatórios de Irrigação</a></li>
                             <li><a href="#obras">Reservatórios de Vinhaça</a></li>
@@ -31,16 +35,16 @@ function Header() {
                             <li><a href="#obras">Biodigestor</a></li>
                         </ul>
                     </li>
-                    <li>
-                        <a href="#orcamentos">Orçamentos</a>
-                    </li>
+                    <li><a href="#orcamentos">Orçamentos</a></li>
                 </ul>
             </nav>
+
+            {/* Botão de contato */}
             <div className="btn">
                 <a href="https://wa.me/5514996228947" target="_blank">Contato</a>
             </div>
         </header>
-    )
+    );
 }
 
 export default Header;
